866eb582d1b948f32ab74669daddeb3d602be14a
YunhaoZZ <zhangenochyun@outlook.com>
2023-02-01 19:51:16 -0600

    Exercise 1

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+  chosenIdx = -1;
+  constructor()
+  {
+  
+  }
+
+  onClick(event: any, idx: number){
+    let chosen = event.target;
+    if(chosen.innerText == "Vote"){
+      this.chosenIdx = idx;
+      chosen.innerText = "Unvote";
+    }else if(chosen.innerTexas = "Unvote"){
+      this.chosenIdx = -1;
+      chosen.innerText = "Vote";
+    }
+     
+  }
+
+  isChosen(idx: number){
+    if(idx == this.chosenIdx)
+      return false;
+    else
+      return true;
+  }
+
+  isDisabled(idx: number){
+    if(this.chosenIdx < 0 || idx==this.chosenIdx)
+      return false;
+    else
+      return true;
+  }
 }
